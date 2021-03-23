33669ac8f55e35fa225ecfeaf42896deb3516d8b
Arinpe <dehtolah@gmail.com>
2021-03-23 19:11:46 +0100

    completed the bot

---
+++ bin/main.rb
@@ -0,0 +1,32 @@
+# !/usr/bin/env ruby
+require 'telegram/bot'
+require 'dotenv/load'
+require_relative '../lib/joke'
+token = ENV['TELEGRAM_TOKEN']
+
+Telegram::Bot::Client.run(token) do |bot|
+    bot.listen do |message|
+      case message.text
+      when '/start'
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, wlecome to telebot #{message.from.first_name}")
+      when '/hi'
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, wlecome to my bot what can I do for you #{message.from.first_name}")
+      when '/help'
+        greet = "Hi, #{message.from.first_name} I am a chatbot and my name TELEBOT
+        \n Commands:
+        \n /start: Greets the user.
+        \n /help: Displays the list of acceptable commands.
+        \n /hi: The bot says hello.
+        \n /hello: The bot says hi.
+        \n /quote: Displays quote.
+        \n /joke: Asks for your name and makes a joke with your name.
+        \n /bye: Says goodbye to the user."
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, wlecome to telebot #{greet}")
+      when '/joke'
+        joke = Joke.new.display_joke
+        bot.api.send_message(chat_id: message.chat.id, text: joke)
+      else
+        bot.api.send_message(chat_id: message.chat.id, text: 'I do not recognize that command')
+      end
+    end
+  end
---
+++ lib/joke.rb
@@ -0,0 +1,22 @@
+require_relative 'request'
+
+class Joke
+  def initialize
+    @joke = ''
+  end
+
+  def display_joke
+    get_joke
+    joke
+  end
+
+  private
+
+  attr_accessor :joke
+
+  def get_joke
+    api = "https://api.chucknorris.io/jokes/random?category=food"
+    res = Request.new_request(api)
+    @joke = res['value']
+  end
+end
